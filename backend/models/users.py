b2a8b186f6096c501a4afdd17e1ee2e016ddb2e1
Micooo24 <mico.rabino24@gmail.com>
2025-07-02 23:38:02 +0800

    additional

--- backend/models/users.py
+++ backend/models/users.py
@@ -4,6 +4,10 @@ from bson import ObjectId
 from typing import Optional, List
 from enum import Enum
 
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
 class Role(str, Enum):
     admin = "admin"
     user = "user"
@@ -13,6 +17,8 @@ class User(BaseModel):
     username: str
     email: str
     password: str
+    img_path: Optional[str] = None
+    gender: Optional[Gender] = None
     role: Role = Role.user
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     
